d43b7ab7c71264f78c2c2e527913c358fe3dd366
DD-SQUARED <drewnare29@gmail.com>
2023-11-28 13:48:33 -0800

    updated

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,29 +1,4 @@
-#include "lists.h"
 
-/**
- * check_cycle - determines whether a linked list contains a cycle
- * @list: linked list checked
- * Return: 1 cycle, otherwise 0
- */
-int check_cycle(listint_t *list)
-{
-        listint_t *slow = list;
-        listint_t *fast = list;
-
-        if (!list)
-                return (0);
-
-        while (slow && fast && fast->next)
-        {
-                slow = slow->next;
-                fast = fast->next->next;
-                if (slow == fast)
-                        return (1);
-        }
-
-        return (0);
-}
-root@e024474929d2:~/alx-higher_level_programming/0x00-python-hello_world# cat lists.h
 #ifndef LISTS_H
 #define LISTS_H
 
